4d468105157eb8926e950a51647c5b7e32e43b95
Kevin Hu <kh2547@nyu.edu>
2018-11-06 18:54:05 -0500

    added solve flag and comments

--- AStarProject/AStarProject/Source.cpp
+++ AStarProject/AStarProject/Source.cpp
@@ -5,20 +5,15 @@
 #include <functional>
 #include <queue>
 
-#define LEFT 'L'
-#define RIGHT 'R'
-#define UP 'U'
-#define DOWN 'D'
-
 using namespace std;
 
 struct PuzzleState {
-	int matrix[3][3];
-	int XPos;
-	int YPos;
+	int matrix[3][3]; // (x,y)
+	int XPos; //Zero XPos
+	int YPos; //Zero YPos
 	int manhattanDistance;
 
-	bool operator==(const PuzzleState& otherState) const {
+	bool operator==(const PuzzleState& otherState) const { // Compares 2 puzzle states
 		for (int i = 0; i < 3; ++i) {
 			for (int j = 0; j < 3; ++j) {
 				if (this->matrix[i][j] != otherState.matrix[i][j]) {
@@ -31,7 +26,7 @@ struct PuzzleState {
 	friend ostream & operator << (ostream &out, const PuzzleState &state);
 };
 
-ostream& operator<<(ostream& out, const PuzzleState& state) {
+ostream& operator<<(ostream& out, const PuzzleState& state) { // output operator for puzzle state 
 	for (int i = 0; i < 3; ++i) {
 		for (int j = 0; j < 3; ++j) {
 			cout << state.matrix[i][j] << " ";
@@ -52,15 +47,15 @@ struct Node {
 	Node(PuzzleState state, char direction, int depth) : state(state), direction(direction), depth(depth) {}
 };
 
-struct cmpNodePtrs
+struct cmpNodePtrs //used as parameter for implementing priority queue
 {
 	bool operator()(const Node* lhs, const Node* rhs) const
 	{
-		return lhs->totalCost > rhs->totalCost;
+		return lhs->totalCost > rhs->totalCost; //allows priority queue to be sorted backwards, smallest cost at the top
 	}
 };
 
-void setState(PuzzleState& initialState, PuzzleState& goalState, string inputFile) {
+void setState(PuzzleState& initialState, PuzzleState& goalState, string inputFile) { //reads the file and sets initial and goal states
 	ifstream ifs(inputFile);
 	string line;
 	int column0, column1, column2;
@@ -79,51 +74,51 @@ void setState(PuzzleState& initialState, PuzzleState& goalState, string inputFil
 	}
 }
 
-PuzzleState MoveUp(const PuzzleState& currState) {
-	PuzzleState nextState = currState;
+PuzzleState MoveUp(const PuzzleState& currState) { //creates new puzzle state based on where zero will go 
+	PuzzleState nextState = currState; //create copy of input state
 	nextState.matrix[nextState.XPos][nextState.YPos] = nextState.matrix[nextState.XPos - 1][nextState.YPos]; //first set what value at 0 will be
 	nextState.matrix[nextState.XPos - 1][nextState.YPos] = 0; //then set new 0
-	nextState.XPos -= 1;
+	nextState.XPos -= 1; //change zero coords
 	return nextState;
 }
 
-PuzzleState& MoveDown(const PuzzleState& currState) {
-	PuzzleState nextState = currState;
-	nextState.matrix[nextState.XPos][nextState.YPos] = nextState.matrix[nextState.XPos + 1][nextState.YPos];
-	nextState.matrix[nextState.XPos + 1][nextState.YPos] = 0;
-	nextState.XPos += 1;
+PuzzleState& MoveDown(const PuzzleState& currState) { //creates new puzzle state based on where zero will go 
+	PuzzleState nextState = currState; //create copy of input state
+	nextState.matrix[nextState.XPos][nextState.YPos] = nextState.matrix[nextState.XPos + 1][nextState.YPos]; //first set what value at 0 will be
+	nextState.matrix[nextState.XPos + 1][nextState.YPos] = 0; //then set new 0
+	nextState.XPos += 1; //change zero coords
 	return nextState;
 }
 
-PuzzleState& MoveLeft(const PuzzleState& currState) {
-	PuzzleState nextState = currState;
-	nextState.matrix[nextState.XPos][nextState.YPos] = nextState.matrix[nextState.XPos][nextState.YPos - 1];
-	nextState.matrix[nextState.XPos][nextState.YPos - 1] = 0;
-	nextState.YPos -= 1;
+PuzzleState& MoveLeft(const PuzzleState& currState) { //creates new puzzle state based on where zero will go 
+	PuzzleState nextState = currState; //create copy of input state
+	nextState.matrix[nextState.XPos][nextState.YPos] = nextState.matrix[nextState.XPos][nextState.YPos - 1]; //first set what value at 0 will be
+	nextState.matrix[nextState.XPos][nextState.YPos - 1] = 0; //then set new 0
+	nextState.YPos -= 1; //change zero coords
+	return nextState;
+}
+ 
+PuzzleState& MoveRight(const PuzzleState& currState) { //creates new puzzle state based on where zero will go 
+	PuzzleState nextState = currState; //create copy of input state
+	nextState.matrix[nextState.XPos][nextState.YPos] = nextState.matrix[nextState.XPos][nextState.YPos + 1]; //first set what value at 0 will be
+	nextState.matrix[nextState.XPos][nextState.YPos + 1] = 0; //then set new 0
+	nextState.YPos += 1; //change zero coords
 	return nextState;
 }
 
-PuzzleState& MoveRight(const PuzzleState& currState) {
-	PuzzleState nextState = currState;
-	nextState.matrix[nextState.XPos][nextState.YPos] = nextState.matrix[nextState.XPos][nextState.YPos + 1];
-	nextState.matrix[nextState.XPos][nextState.YPos + 1] = 0;
-	nextState.YPos += 1;
-	return nextState;
-}
-
-bool checkDupe(PuzzleState& newState, vector<Node*> visited) {
+bool checkDupe(PuzzleState& newState, vector<Node*> visited) { //checks if a state is already in visited array
 	for (Node* nodeCheck : visited) {
 		if (nodeCheck->state == newState) {
-			return true;
+			return true; //if state has been visited, return true
 		}
 	}
-	return false;
+	return false; //if state has not been visited, return false
 }
 
 void calculateMD(PuzzleState& state, const PuzzleState& goalState) { //calculates manhattan distance and updates
 	int MD = 0;
 	int x, y, x2, y2; // (x2,y2) are goal coords, (x,y2) are state coords
-	for (int i = 0; i < 9; ++i) {
+	for (int i = 0; i < 9; ++i) { //iterates 9 times, to calculate manhattan distance of each value 0-8
 		for (int j = 0; j < 3; ++j) {
 			for (int k = 0; k < 3; ++k) {
 				if (state.matrix[j][k] == i) {
@@ -136,18 +131,16 @@ void calculateMD(PuzzleState& state, const PuzzleState& goalState) { //calculate
 				}
 			}
 		}
-		MD += (abs(x - x2) + abs(y - y2));
+		MD += (abs(x - x2) + abs(y - y2)); //repeatedly increases manhattan distance
 	}
-	
-	state.manhattanDistance = MD;
-
+	state.manhattanDistance = MD; //sets manhattan distance
 }
 
-void checkValidandRun(Node& currNode, PuzzleState& goalState, priority_queue<Node*, vector<Node*>, cmpNodePtrs>& frontier, const vector<Node*>& visited) {
-	if (currNode.state.YPos > 0) {
-		PuzzleState newState = MoveLeft(currNode.state);
-		if (checkDupe(newState, visited) == false) {
-			calculateMD(newState, goalState);
+void expand(Node& currNode, PuzzleState& goalState, priority_queue<Node*, vector<Node*>, cmpNodePtrs>& frontier, const vector<Node*>& visited) { //expands possible routes for node
+	if (currNode.state.YPos > 0) { //checks if zero can move left
+		PuzzleState newState = MoveLeft(currNode.state); 
+		if (checkDupe(newState, visited) == false) { 
+			calculateMD(newState, goalState); 
 			Node* childNode = new Node(newState, 'L', currNode.depth + 1);
 			childNode->totalCost = childNode->depth + childNode->state.manhattanDistance;
 			childNode->parent = &currNode;
@@ -155,7 +148,7 @@ void checkValidandRun(Node& currNode, PuzzleState& goalState, priority_queue<Nod
 			frontier.push(childNode);
 		}
 	}
-	if (currNode.state.YPos < 2) {
+	if (currNode.state.YPos < 2) {//checks if zero can move right
 		PuzzleState newState = MoveRight(currNode.state);
 		if (checkDupe(newState, visited) == false) {
 			calculateMD(newState, goalState);
@@ -166,7 +159,7 @@ void checkValidandRun(Node& currNode, PuzzleState& goalState, priority_queue<Nod
 			frontier.push(childNode);
 		}
 	}
-	if (currNode.state.XPos < 2) {
+	if (currNode.state.XPos < 2) { //checks if zero can move down
 		PuzzleState newState = MoveDown(currNode.state);
 		if (checkDupe(newState, visited) == false) {
 			calculateMD(newState, goalState);
@@ -177,7 +170,7 @@ void checkValidandRun(Node& currNode, PuzzleState& goalState, priority_queue<Nod
 			frontier.push(childNode);
 		}
 	}
-	if (currNode.state.XPos> 0) {
+	if (currNode.state.XPos> 0) { //checks if zero can move up
 		PuzzleState newState = MoveUp(currNode.state);
 		if (checkDupe(newState, visited) == false) {
 			calculateMD(newState, goalState);
@@ -201,7 +194,7 @@ void setZero(PuzzleState& initialState) { //finds and sets initial zero location
 	}
 }
 
-void printPath(Node* goalNode) {
+void printPath(Node* goalNode) { //recursively prints out path to reach goal
 	if (goalNode->direction == NULL) {
 		return;
 	}
@@ -217,30 +210,32 @@ int main() {
 	setZero(initialState); //locates and sets zero of initial
 	calculateMD(initialState, goalState); //calculates and sets initial MD
 
-	priority_queue<Node*, vector<Node*>, cmpNodePtrs> frontier;
-	vector<Node*> visited;
+	priority_queue<Node*, vector<Node*>, cmpNodePtrs> frontier; //nodes that are going to be visited
+	vector<Node*> visited; //nodes that have been visited
 	
 	Node* initialNode = new Node(initialState, 0, NULL);
 	frontier.push(initialNode);
 	Node* nextNode;
-	int totalStates = frontier.size() + visited.size();
+	int totalStates = frontier.size() + visited.size(); //total number of states 
+	bool solved = false; //is solved flag
 
-	while (frontier.empty() == false) {
+	while (frontier.empty() == false) { //must visit everything in frontier
 		nextNode = frontier.top();
 		visited.push_back(nextNode);
 		frontier.pop();
-		if (nextNode->state == goalState) {
+		if (nextNode->state == goalState) { //if it reaches goal, break
 			printPath(nextNode);
 			cout << endl;
 
 			cout << "counter " << frontier.size() + visited.size() << endl;
+			bool solved = true; //solution is found
 			break;
 		}
-
-		checkValidandRun(*nextNode, goalState, frontier, visited);
+		expand(*nextNode, goalState, frontier, visited);
 	}
 
+	if (!solved) {
+		cout << " no solution " << endl;
+	}
 	getchar();
-	
-	
 }
